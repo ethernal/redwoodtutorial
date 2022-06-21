d6e7a507275218bfb0ce23a04984f7b3adf40411
Sebastian <sebastian.pieczynski@morecode.ai>
2022-06-21 13:09:30 +0200

    Chapter 2: create Post and display it on home page

---
+++ web/src/components/ArticlesCell/ArticlesCell.js
@@ -0,0 +1,40 @@
+export const QUERY = gql`
+  query ArticlesQuery {
+    articles: posts {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`;
+
+export const Loading = () => <div>Loading...</div>;
+
+export const Empty = () => <div>Empty</div>;
+
+export const Failure = ({ error }) => (
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
+);
+
+export const Success = ({ articles }) => {
+  return (
+    <>
+      {articles.map((article) => {
+        return (
+        <article key={article.id}>
+          <header>
+            <h2>{article.title}</h2>
+          </header>
+          <p>
+            {article.body}
+          </p>
+          <div>
+            Posted: {article.createdAt}
+          </div>
+        </article>
+          );
+      })}
+    </>
+  );
+};
