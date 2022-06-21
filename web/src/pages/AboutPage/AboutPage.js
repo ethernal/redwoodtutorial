bf82d2ee4be773dbc2a5719b0d3d750abbbd4b76
Sebastian <sebastian.pieczynski@morecode.ai>
2022-06-21 12:05:02 +0200

    Initial commit: Chapter 1 complete

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,16 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      <h2>
+        About Blog
+      </h2>
+    </>
+  )
+}
+
+export default AboutPage
